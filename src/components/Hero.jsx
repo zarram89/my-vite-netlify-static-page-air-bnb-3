6d09c52017ae0a614e87edfb2d26a1defc146272
Ramil <zarram89@gmail.com>
2022-12-10 22:27:32 +0500

    Adds hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,10 @@
 function Hero() {
   return (
     <div className="hero">
-      <img src="./promo.png" width="395" height="232" alt="Promo photo" />
+      <img className="hero-img" src="./promo.png" width="395" height="232" alt="Promo photo" />
+      <h1 className="hero-title">Online Experiences</h1>
+      <p className="hero-text">Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.
+      </p>
     </div>
   )
 }
