a02ce1111ed43a082ffea902460a5b486213c229
Ramil <zarram89@gmail.com>
2022-12-10 21:59:29 +0500

    Adds navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+function Navbar() {
+  return (
+    <nav className="nav">
+      <img className="logo" src="./logo.svg" width="82" height="25" alt=" Airbnb logo" />
+    </nav>
+  )
+}
+
+export default Navbar
